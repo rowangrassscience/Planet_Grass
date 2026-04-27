1ee38248cb07c8ee65914d49b7bb2e087d4709ba
rowangrassscience <rowan.grass.science@gmail.com>
2026-04-27 16:03:22 +0100

    Update spritespin.js

--- spritespin/spritespin.js
+++ spritespin/spritespin.js
@@ -1,1 +1,94 @@
+(function(global, $){
+
+  function SpriteSpin(opts){
+    this.opts = opts;
+    this.$target = opts.target;
+    this.frames = opts.source || [];
+    this.frame = 0;
+    this.zoom = 1;
+    this.dragging = false;
+    this.lastX = 0;
+    this.init();
+  }
+
+  SpriteSpin.prototype.init = function(){
+    var self = this;
+
+    // Create canvas
+    self.canvas = document.createElement("canvas");
+    self.ctx = self.canvas.getContext("2d");
+    self.$target.append(self.canvas);
+
+    // Resize
+    function resize(){
+      self.canvas.width = self.$target.width();
+      self.canvas.height = self.$target.height();
+      self.draw();
+    }
+    $(window).on("resize", resize);
+    resize();
+
+    // Load first frame
+    self.images = [];
+    self.frames.forEach(function(src, i){
+      var img = new Image();
+      img.onload = function(){
+        self.images[i] = img;
+        if(i === 0) self.draw();
+      };
+      img.src = src;
+    });
+
+    // Drag
+    self.$target.on("mousedown touchstart", function(e){
+      self.dragging = true;
+      self.lastX = e.pageX || e.originalEvent.touches[0].pageX;
+    });
+
+    $(window).on("mouseup touchend", function(){
+      self.dragging = false;
+    });
+
+    self.$target.on("mousemove touchmove", function(e){
+      if(!self.dragging) return;
+      var x = e.pageX || e.originalEvent.touches[0].pageX;
+      var dx = x - self.lastX;
+      self.lastX = x;
+      self.frame = (self.frame - Math.sign(dx) + self.frames.length) % self.frames.length;
+      self.draw();
+    });
+
+    // Zoom
+    self.$target.on("wheel", function(e){
+      e.preventDefault();
+      self.zoom += (e.originalEvent.deltaY < 0 ? 0.1 : -0.1);
+      self.zoom = Math.max(self.opts.zoomMin || 1, Math.min(self.opts.zoomMax || 3, self.zoom));
+      self.draw();
+    });
+  };
+
+  SpriteSpin.prototype.draw = function(){
+    var img = this.images[this.frame];
+    if(!img) return;
+
+    var ctx = this.ctx;
+    var w = this.canvas.width;
+    var h = this.canvas.height;
+
+    ctx.clearRect(0,0,w,h);
+
+    var scale = this.zoom;
+    var iw = img.width * scale;
+    var ih = img.height * scale;
+
+    ctx.drawImage(img, (w - iw)/2, (h - ih)/2, iw, ih);
+  };
+
+  SpriteSpin.create = function(opts){
+    return new SpriteSpin(opts);
+  };
+
+  global.SpriteSpin = SpriteSpin;
+
+})(window, jQuery);
 
